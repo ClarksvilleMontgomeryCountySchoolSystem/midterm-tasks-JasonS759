b11c4cf7a7fd1a9fca60a23cdddfeb8ee3d2053e
JasonS759 <jason.sakwe759@students.cmcss.net>
2025-12-18 09:22:18 -0600

    Update task1.py

--- task1.py
+++ task1.py
@@ -1,1 +1,31 @@
+#Given variables
+party_pizza_mini = 14
+large = 8
+medium = 6
+people = 6 # friends
 
+#Do Not use crtl+A to select code.  Only copy the code below this line.
+#------------------------------------------------------------------------------------------------
+slices = party_pizza_mini + large + medium
+print(f"Total Number of slices: {slices})")
+
+people+=1
+share = slices // people
+leftover = slices % people
+print(f"Each person gets: {share}")
+print(f"Leftover slices: {leftover}")
+
+people+=2 #Eric and Brandon are coming too.
+share=slices//people
+leftover = slices % people
+print(f"Each person gets: {share}")
+print(f"Leftover slices: {leftover}")
+
+
+
+slices = large + medium + (party_pizza_mini * 2)
+share = slices //people
+leftover = slices % people
+print(f"Each person gets: {share}")
+print(f"Leftover slices: {leftover}")
+print("...for Mr. Hollow Leg")
